cbe4ddb8aee676d6fb2d4266d1b546338e8ff000
rucker <rick.ucker@gmail.com>
2015-06-20 13:28:32 -0400

    Add install script (initial commit).

---
+++ install_dotfiles.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#  install_dotfiles
+#  This script will build platform-specific dotfiles and create the appropriate symlinks in ~
+
+import platform
+import os
+
+sysName = platform.system()
+os.remove('bashrc')
+bashrc = open('bashrc','a')
+
+def writeSection(fileName):
+  f = open(fileName,'r')
+  for line in f:
+    bashrc.write(line)
+
+writeSection('bash_common')
+if os.path.isfile('bash_private'):
+  writeSection('bash_private')
+if sysName == 'Linux':
+  writeSection('bash_linux')
+elif sysName == 'Darwin':
+  writeSection('bash_mac')
+else:
+  print "System not supported!"
+  bashrc.close()
+  exit(1)
+bashrc.close()
+
