825afc1fdf2146bd27c4cefae5b85f4729d4b557
rucker <rick.ucker@gmail.com>
2015-06-23 10:28:02 -0400

    Test: installer should identify Darwin systems correctly.

--- test_dotfiles.py
+++ test_dotfiles.py
@@ -4,11 +4,13 @@ import unittest
 import mock
 import dotfilesinstaller
 import platform
+import sys
 
 class DotfilesTest(unittest.TestCase):
   @mock.patch('platform.system', mock.MagicMock(return_value='Darwin'))
-  def testWhenSystemIsDarwinInstallerIdentifiesSystemIsDarwin(self):
-      print dotfilesinstaller.identifySystem()
+  def testWhenSystemIsDarwinInstallerIdentifiesSystemAsDarwin(self):
+    dotfilesinstaller.identifySystem()
+    assert(sys.stdout.getvalue().strip().endswith('Darwin'))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(DotfilesTest)
-unittest.TextTestRunner().run(suite)
+unittest.main(module=__name__, buffer=True, exit=False)
