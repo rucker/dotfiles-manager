41c7dc3ac17a43508c48ca0428a80fea3d110e99
rucker <rick.ucker@gmail.com>
2015-06-23 11:02:50 -0400

    Test: if bashrc exists, delete it.

--- test_dotfiles.py
+++ test_dotfiles.py
@@ -5,8 +5,12 @@ import mock
 import dotfilesinstaller
 import platform
 import sys
+import os
 
 class DotfilesTest(unittest.TestCase):
+  def setUp(self):
+    dotfilesinstaller.init()
+
   @mock.patch('platform.system', mock.MagicMock(return_value='Darwin'))
   def testWhenSystemIsDarwinInstallerIdentifiesSystemAsDarwin(self):
     dotfilesinstaller.identifySystem()
@@ -23,6 +27,15 @@ class DotfilesTest(unittest.TestCase):
       dotfilesinstaller.identifySystem()
       assert(sys.stdout.getvalue().strip().endswith('not supported!'))
       assertEqual(cm.exception.code, 1)
+
+  def testWhenBashrcExistsInstallerWillDeleteIt(self):
+    if not os.path.isfile('bashrc'):
+      testbashrc = open('bashrc','w')
+      testbashrc.write('Test file...')
+      testbashrc.close()
+    dotfilesinstaller.cleanUp()
+    assert(sys.stdout.getvalue().strip().endswith('bashrc'))
+    self.assertFalse(os.path.isfile('bashrc'))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(DotfilesTest)
 unittest.main(module=__name__, buffer=True, exit=False)
