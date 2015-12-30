28413a501e849e224b528da12b526563ead5ec21
rick.ucker@gmail.com <rick.ucker@gmail.com>
2015-12-30 11:07:25 -0500

    Write bash file header.

--- dotfiles/dotfiles.py
+++ dotfiles/dotfiles.py
@@ -2,15 +2,14 @@
 
 import sys
 import platform
+import io
 from systems import Systems
-
 
 def identifySystem():
   global sysName
-  
   supportedPlatforms = [Systems.DARWIN.value, Systems.LINUX.value]
-
   sysName = platform.system()
+
   if sysName not in supportedPlatforms:
     print "System not supported!"
     exit(1)
@@ -18,3 +17,5 @@ def identifySystem():
 
 if __name__ == '__main__':
   identifySystem()
+  with io.StringIO() as fileBuffer:
+    bashfile.writeHeader(sysName, fileBuffer)
