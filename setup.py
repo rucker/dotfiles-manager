b718a5ba29c5449dcc234cc6158450b1817654bc
Rick Ucker <rick.ucker@gmail.com>
2016-01-27 18:41:05 -0500

    Move setup file to project root as per convention.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = 'dotfiles',
+      version = '2.1',
+      description = 'A dotfile compilation/setup utility.',
+      author = 'Rick Ucker',
+      url = 'https://github.com/rucker/dotfiles',
+      license = 'MIT',
+      packages = ['dotfiles','dotfiles/test'],
+      requires = ['mock','enum34'],
+     )
